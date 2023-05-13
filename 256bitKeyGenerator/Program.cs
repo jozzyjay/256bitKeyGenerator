8bf6810bcf20e92fce11464832597adb8503c556
Joshua Ayodeji <jayodeji@gtlregistrars.com>
2023-05-13 14:13:26 +0100

    Add project files.

---
+++ 256bitKeyGenerator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace RandomKeyGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string randomKey = GenerateRandomKey(256);
+            Console.WriteLine("Random Key: " + randomKey);
+        }
+
+        static string GenerateRandomKey(int keySizeInBits)
+        {
+            byte[] key = new byte[keySizeInBits / 8];
+
+            using (var rngProvider = new RNGCryptoServiceProvider())
+            {
+                rngProvider.GetBytes(key);
+            }
+
+            return Convert.ToBase64String(key);
+        }
+    }
+}
